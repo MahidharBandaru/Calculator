889aae451bd3741ef720c7f15ded56edf46ad876
Mahidhar <mahi.mahidhar2@gmail.com>
2020-06-29 17:19:58 +0530

    Add subtract

--- calc.py
+++ calc.py
@@ -1,9 +1,16 @@
 def add(a, b):
     return a + b
 
+def subtract(a, b):
+    return a - b
+
 if __name__ == "__main__":
     oprnd = "+";
     a = 2
     b = 3
     if oprnd == "+":
         print(add(a, b))
+    if oprnd == "+":
+        print(add(a, b))
+    if oprnd == "-":
+        print(subtract(a, b))
--- test_calc.py
+++ test_calc.py
@@ -2,3 +2,6 @@ from calc import *
 
 def test_add():
     assert add(5, 3) == 8
+
+def test_subtract():
+    assert subtract(5, 3) == 2
